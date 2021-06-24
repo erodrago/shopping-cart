79671d6486be314b9d27a71169d285fdff9a135a
ero <eric.ndira@gmail.com>
2021-06-24 09:00:01 +0300

    chore: added cart items table

---
+++ src/database/models/cartitem.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class CartItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  CartItem.init({
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'cart_items',
+    modelName: 'CartItem',
+  });
+  return CartItem;
+};
