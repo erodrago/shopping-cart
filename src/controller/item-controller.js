52a0cad973ce6aa3e776b4e34cda3519a0409c67
ero <eric.ndira@gmail.com>
2021-06-25 12:00:58 +0300

    chore : added functionality to remove an item from cart

--- src/controller/item-controller.js
+++ src/controller/item-controller.js
@@ -3,7 +3,7 @@ const { CartItem, OrderItem, CartSession} = require('../database/models');
 exports.addItemToCart = async (req, res) => {
     const { quantity, amount, productId } = req.body;
     const { sessionId } = req.params;
-    
+
     // validate body
     if (!quantity || !amount || !productId || !sessionId) {
         return res.status(400).send({
@@ -41,6 +41,50 @@ exports.addItemToCart = async (req, res) => {
             session_id: sessionId
         });
         return res.send(newCartItem);
+    } catch (err) {
+        return res.status(500).send({
+            message: `Error: ${err.message}`,
+        });
+    }
+};
+
+exports.removeItemFromCart = async (req, res) => {
+    const { sessionId, id } = req.params;
+
+    if (!id || sessionId) {
+        return res.status(400).send({
+            message: 'Please provide sessionId and ID of item to delete',
+        });
+    }
+
+    // check if there is an existing session exists
+    const session = await CartSession.findOne({
+        where: {id: sessionId}
+    });
+
+    if(!session){
+        return res.status(404).send({
+            message: `The session does not exist please log in`
+        })
+    }
+
+    const cartItem = await CartItem.findOne({
+        where: {
+            id,
+        },
+    });
+
+    if (!cartItem) {
+        return res.status(404).send({
+            message: `Delete unsuccesful!! No item found `,
+        });
+    }
+
+    try {
+        await cartItem.destroy();
+        return res.send({
+            message: `Item ${id} has been deleted!`,
+        });
     } catch (err) {
         return res.status(500).send({
             message: `Error: ${err.message}`,
